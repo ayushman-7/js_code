1162c1cd476daa22384090ebae4c6f13567f693c
ayushman-7 <127099388+ayushman-7@users.noreply.github.com>
2024-05-07 05:42:57 +0000

    go

---
+++ 01_Basics/03_conversions.js
@@ -0,0 +1,19 @@
+let score="33"
+
+console.log(typeof score);
+
+let valueInNumber= Number(score);
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+    
+
+let islogged= ""
+
+let booleanislogged= Boolean(islogged)
+console.log(booleanislogged)
+
+
+let some=33
+let stringno= String(some)
+console.log(stringno)
+console.log(typeof stringno)
