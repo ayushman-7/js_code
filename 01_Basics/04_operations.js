1326fae1c3c33c8b82525abbea02ece07ee5afe0
ayushman-7 <127099388+ayushman-7@users.noreply.github.com>
2024-05-07 05:59:32 +0000

    good

---
+++ 01_Basics/04_operations.js
@@ -0,0 +1,14 @@
+let value =3
+let negative=-value
+console.log(negative)
+
+let a="hi"
+let b=" ayushman"
+let c= a+b
+console.log(c)
+console.log(+true)
+console.log(+"")
+
+let p=100
+++p;
+console.log(p)
