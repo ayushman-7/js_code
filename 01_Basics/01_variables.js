a438308af2b5b0cb85655c6618d03e82ec971032
ayushman-7 <127099388+ayushman-7@users.noreply.github.com>
2024-05-07 05:27:44 +0000

    yes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,5 @@
+const accountId=14453
+let accountemail="ayus@google"
+var accountpass="12345"
+let accountstate;
+ console.table([accountId,accountemail,accountpass,accountstate])
---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let name="ayushman"
+let age=3
+let isloggedin=true
+
+
+// null= standalone value
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
