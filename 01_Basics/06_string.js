95daec547b8df2bd6e326600ce9b7830cceb8dfe
ayushman-7 <127099388+ayushman-7@users.noreply.github.com>
2024-05-11 13:12:37 +0000

    after 2 days

---
+++ 01_Basics/06_string.js
@@ -0,0 +1,6 @@
+const name= new String('Ayushman')
+console.log(name[0]);
+console.log(name.__proto__);
+
+console.log(name.length);
+console.log(`${name} is a good boy`)
---
+++ 01_Basics/07_mumsandmath.js
@@ -0,0 +1,22 @@
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toString());
+
+console.log(balance.toFixed(2));
+
+ const hundreds=10000000
+ console.log(hundreds.toLocaleString('en-IN'));
+
+//********************MAths */////////////\
+
+console.log(Math)
+
+console.log(Math.round(4.8))
+
+console.log((Math.random()*10)+1);
+
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
