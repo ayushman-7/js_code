375f893a39db1eb359588f50050a9bedc0aca08c
ayushman-7 <127099388+ayushman-7@users.noreply.github.com>
2024-05-18 05:26:55 +0000

    objects complete

--- 02_Basics/02_objects.js
+++ 02_Basics/02_objects.js
@@ -1,29 +1,63 @@
+// //singlton
+
+// // object literals
+// //Object.create
+
+// const mysym=Symbol("key1")
+// const jsuser={
+//     name:"Ayushman",
+//     age:20,
+//     [mysym]:"abc",
+//     "email":"abc@google.com",
+//     location: "Alllahabad"
+// }
+
+// // console.log(jsuser.age)
+// // console.log(jsuser["email"])
+// // console.log(jsuser[mysym])
+
+// jsuser.age=23
+// //Object.freeze(jsuser)
+// jsuser.age=87
+// //console.log(jsuser)
+
+
+// jsuser.greeting= function(){
+//     console.log(`hii!!, ${this.name}`)
+// }
+
+// console.log(jsuser.greeting());
+
 //singlton
 
-// object literals
-//Object.create
-
-const mysym=Symbol("key1")
-const jsuser={
-    name:"Ayushman",
-    age:20,
-    [mysym]:"abc",
-    "email":"abc@google.com",
-    location: "Alllahabad"
+//const tinder =new Object() // singlton
+const tinder = {}  //non singlton
+tinder.id="abc123"
+tinder.name="ABC"
+console.log(tinder)
+const regular={
+    email:"abc@123",
+    fullname:{
+        username:{
+            firstname:"Ayushman",
+            lastname:"Tripathi"
+        }
+    }
 }
+console.log(Object.keys(tinder))
+console.log(Object.values(tinder))
+console.log(Object.entries(tinder))
+console.log(tinder.hasOwnProperty('id'))
 
-// console.log(jsuser.age)
-// console.log(jsuser["email"])
-// console.log(jsuser[mysym])
-
-jsuser.age=23
-//Object.freeze(jsuser)
-jsuser.age=87
-//console.log(jsuser)
+// console.log(regular.fullname.username.lastname)
 
 
-jsuser.greeting= function(){
-    console.log(`hii!!, ${this.name}`)
-}
+const obj1={1:"a", 2:"b"}
+const obj2={3:"a", 4:"b"}
+const ob4={...obj1,...obj2}
+console.log(ob4)
+const ob3=Object.assign(obj1,obj2)
+console.log(ob3);
+console.log(obj1);
 
-console.log(jsuser.greeting());
+
