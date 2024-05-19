fc0d44f6b73298803bf194711dbf0e82a2bf9b55
ayushman-7 <127099388+ayushman-7@users.noreply.github.com>
2024-05-19 06:34:49 +0000

    partially done

--- 03_Basics/01_functions.js
+++ 03_Basics/01_functions.js
@@ -1,24 +1,53 @@
-function sayMyName(){
-    console.log("Hello")
-    console.log("Ayushman")
-}
+// function sayMyName(){
+//     console.log("Hello")
+//     console.log("Ayushman")
+// }
 
-sayMyName()
-let result;
-function add(num1,num2){
-    return num1+num2;
+// sayMyName()
+// let result;
+// function add(num1,num2){
+//     return num1+num2;
     
+// }
+
+// result=add(2,3)
+// console.log(result)
+
+// function login(username="Ayushman"){
+//     if(!username){
+//         console.log("please enter username ");
+//         return
+//     }
+//     return`${username} just logged in`
+// }
+
+// console.log(login())
+
+
+function cartvalue(val1,val2,...numb1){
+    return numb1
+
+}
+console.log(cartvalue(200,400,500))
+
+
+
+const user={
+    username1:"ayu",
+    price:"65"
 }
 
-result=add(2,3)
-console.log(result)
+function handleobject(anyobject){
+    console.log(`username is ${anyobject.username1} and price is ${anyobject.price}`);
 
-function login(username="Ayushman"){
-    if(!username){
-        console.log("please enter username ");
-        return
-    }
-    return`${username} just logged in`
+
 }
+handleobject(user)
 
-console.log(login())
+const newarray=[3885,36,565]
+
+function arrayre(getarray){
+    return getarray[1]
+
+}
+console.log(arrayre(newarray))
