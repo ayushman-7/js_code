670ac3e9dfe3b7131521bd6fb68d480ed9a5f233
AYUSHMAN TRIPATHI <127099388+ayushman-7@users.noreply.github.com>
2024-05-21 15:09:29 +0000

    done

---
+++ 04_control_flow/01.js
@@ -0,0 +1,18 @@
+// if statement
+
+const userlogged=true
+if(2==='2'){
+    console.log("executed");
+}
+
+
+const user=true
+const name=true
+const last=false
+const first=true
+if(user&&name&&2==3){
+    console.log("allow")
+}
+if(last||first){
+    console.log("check")
+}
