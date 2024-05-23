601859bbadaaa8bbe677301cf04b9815457a2d94
AYUSHMAN TRIPATHI <127099388+ayushman-7@users.noreply.github.com>
2024-05-23 04:47:33 +0000

    for loop done

---
+++ 05_itterations/01.js
@@ -0,0 +1,15 @@
+//for
+for(let i=0;i<=10;i++){
+    //console.log(`outer loop ${i}`)
+    for(let j=0;j<=10;j++){
+        //console.log(`inner loop ${j}`)
+    }
+}
+
+
+let myarray=["abc","bfjb","jbuvb"]
+
+for(let i=0;i<myarray.length;i++){
+    const elememt=myarray[i];
+    console.log(elememt)
+}
