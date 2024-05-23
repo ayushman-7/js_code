5783b04c8be251b0efa1ea2d00d227a9d86edfcf
AYUSHMAN TRIPATHI <127099388+ayushman-7@users.noreply.github.com>
2024-05-23 05:01:46 +0000

    do while done

---
+++ 05_itterations/02.js
@@ -0,0 +1,20 @@
+//while
+let i=0
+while(i<=10){
+    console.log(i)
+    i=i+2
+}
+
+let myarray=["dvdv","def","ccdee"]
+let arr=0
+while(arr<myarray.length){
+    console.log(myarray[arr]);
+    arr=arr+1
+}
+
+let score=11
+
+do{
+    console.log(score)
+    score++
+}while(score<=10);
